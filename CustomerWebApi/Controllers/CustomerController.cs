8a4e7df6218cf64dd7667d6c16b9a8930d4766ce
omarsilvayepes <omar.yepes@hotmail.com>
2022-10-11 21:27:33 -0500

    Created repository

---
+++ CustomerWebApi/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using CustomerWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerDbContext _customerDbContext;
+        public CustomerController(CustomerDbContext customerDbContext)
+        {
+            this._customerDbContext = customerDbContext;// use dependency Injection
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> GetCustomers()
+        {
+            return _customerDbContext.Customers;
+        }
+
+        [HttpGet("{customerId:int}")]
+        public async Task<ActionResult<Customer>> GetCustomerByIdAsync(int customerId)
+        {
+             return await _customerDbContext.Customers.FindAsync(customerId);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateCustomerAsync(Customer customer)
+        {
+            await _customerDbContext.Customers.AddAsync(customer);
+            await _customerDbContext.SaveChangesAsync();
+            return Ok("Saved Sucessfully");
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateCustomerAsync(Customer customer)
+        {
+            _customerDbContext.Update(customer);
+            await _customerDbContext.SaveChangesAsync();
+            return Ok("Update Sucessfully");
+        }
+
+        [HttpDelete("{customer:int}")]
+        public async Task<ActionResult> DeleteCustomerAsync(int customerId)
+        {
+            var customer = await _customerDbContext.Customers.FindAsync(customerId);
+            _customerDbContext.Remove(customer);
+            await _customerDbContext.SaveChangesAsync();
+            return Ok("Delete Sucessfully");
+        }
+    }
+}
---
+++ CustomerWebApi/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CustomerWebApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration=configuration;
+        }
+
+   
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            /* Database Context Dependency Injection */
+
+            services.AddDbContext<CustomerDbContext>(options=> options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
