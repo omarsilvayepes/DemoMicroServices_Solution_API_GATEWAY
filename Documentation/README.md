d3f4f1fc32b9c372e391c806d3aa82ec98dac85d
omarsilvayepes <omar.yepes@hotmail.com>
2022-10-24 17:35:49 -0500

    add documentation anf orchestration for api gateway

---
+++ Documentation/README.md
@@ -0,0 +1,25 @@
+# Project Title
+
+.NET Core Demo Microservice Solution
+
+## Description
+
+This solution is a collection of two  microservices with different database (SQL Server, MySQL). We'll be developing a API Gateway which communicates with all the two microservices for doing the CRUD operations.
+
+Tutorial Videos Base:
+
+.NET Core Web API Microservice with SQL Server Entity Framework Core - https://www.youtube.com/watch?v=2p01iafOxUw
+.Net Core MySQL Microservice - Entity Framework Core MySQL - https://www.youtube.com/watch?v=b1BSu0Wb2Rw
+ASP.Net Core API Gateway - Ocelot API Microservice - https://www.youtube.com/watch?v=k4l3Ptd4yjw
+
+
+## Authors
+
+Omar Silva Yepes 
+[@Omar](omar.yepes@hotmail.com)
+
+## Version History
+
+* 0.1
+    * Initial Release
+
--- ProductWebApi/Startup.cs
+++ ProductWebApi/Startup.cs
@@ -17,7 +17,18 @@ namespace ProductWebApi
 
             /* Database Context Dependency Injection */
 
-            services.AddDbContext<ProductDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("DevConnection")));
+            //Without Docker compose use the follow:
+
+            //services.AddDbContext<ProductDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("DevConnection")));
+
+            //With Docker compose use the follow:
+
+            var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+            var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+            var dbPassword = Environment.GetEnvironmentVariable("DB_ROOT_PASSWORD");
+            var connectionString = $"server={dbHost};port=3306;database={dbName};user=root;password={dbPassword}";
+            services.AddDbContext<ProductDbContext>(options => options.UseMySQL(connectionString));
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
